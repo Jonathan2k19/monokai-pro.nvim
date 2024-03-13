a6f02923904bd1d85750b6eb42ae971d04874d58
Jonathan2k19 <goprokonto4@gmail.com>
2024-03-13 15:22:59 +0100

    Adjust light color palette

--- lua/monokai-pro/colorscheme/palette/light.lua
+++ lua/monokai-pro/colorscheme/palette/light.lua
@@ -1,23 +1,18 @@
 ---@type Palette
 return {
-    -- Black-ish (order: dark -> light)
-    dark2 = "#fffffa", -- statusbar background
-    dark1 = "#ff00ff",
-    background = "#fffffa",
-
-    text = "#0d0d0d",
-
-    accent1 = "#ff004b", -- Magenta
-    accent2 = "#ff8f00", -- Orange
-    accent3 = "#ffbf00", -- Yellow
-    accent4 = "#4fb000", -- Green
-    accent5 = "#00b3e3", -- Blue
-    accent6 = "#6054d0", -- Purple
-
-    -- Grey-ish (order: light -> dark)
-    dimmed1 = "#c0c1b5", -- visual selection
-    dimmed2 = "#919288",
-    dimmed3 = "#6e7066", -- comments
-    dimmed4 = "#57584f",
-    dimmed5 = "#3b3c35",
+  dark2 = "#000000",      -- statusbar background
+  dark1 = "#000000",      -- telescope header fg, ...
+  background = "#ffffff",
+  text = "#0d0d0d",
+  accent1 = "#ff004b",    -- Magenta
+  accent2 = "#ff8f00",    -- Orange
+  accent3 = "#ffbf00",    -- Yellow
+  accent4 = "#4fb000",    -- Green
+  accent5 = "#00b3e3",    -- Blue
+  accent6 = "#6054d0",    -- Purple
+  dimmed1 = "#c0c1b5",    -- visual selection
+  dimmed2 = "#0d0d0d",    -- telescope fg/border, ...
+  dimmed3 = "#6e7066",    -- comments
+  dimmed4 = "#57584f",    -- ?
+  dimmed5 = "#3b3c35",    -- ?
 }
